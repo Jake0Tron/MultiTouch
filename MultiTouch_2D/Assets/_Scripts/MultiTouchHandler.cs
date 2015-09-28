bc6d330c9d250d431ae712a0a83d8a30162c5a4d
Jake Deacon <deaconj02@gmail.com>
2015-09-28 10:15:01 -0400

    Setting up prefabs
    Added prefab and instantiate code

--- MultiTouch_2D/Assets/_Scripts/MultiTouchHandler.cs
+++ MultiTouch_2D/Assets/_Scripts/MultiTouchHandler.cs
@@ -1,20 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MultiTouchHandler : MonoBehaviour {
 
+    public Text debugText;
+    public bool debug;
+    public GameObject spawnItem;
+
 	// Use this for initialization
 	void Start () {
-	
+        this.debug = true;
+        this.debugText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
         var tapCount = Input.touchCount;
+
         for (var i = 0; i < tapCount; i++)
         {
             var touch = Input.GetTouch(i);
-
+            Instantiate(spawnItem, new Vector3(touch.position.x,touch.position.y), Quaternion.identity);
+            
         }
+
 	}
 }

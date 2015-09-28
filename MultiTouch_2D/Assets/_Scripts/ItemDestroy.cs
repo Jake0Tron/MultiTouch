9afd54d292d05f5fb3463b134d32318ace147c74
Jake Deacon <deaconj02@gmail.com>
2015-09-28 11:07:06 -0400

    Cleanup and debug testing

---
+++ MultiTouch_2D/Assets/_Scripts/ItemDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine(Destroy());
+	}
+
+	public IEnumerator Destroy()
+	{
+		yield return new WaitForSeconds(3.0f);
+		Destroy(this.gameObject);
+	}
+}
--- MultiTouch_2D/Assets/_Scripts/MultiTouchHandler.cs
+++ MultiTouch_2D/Assets/_Scripts/MultiTouchHandler.cs
@@ -8,6 +8,8 @@ public class MultiTouchHandler : MonoBehaviour {
     public bool debug;
     public GameObject spawnItem;
 
+
+
 	// Use this for initialization
 	void Start () {
         this.debug = true;
@@ -20,10 +22,20 @@ public class MultiTouchHandler : MonoBehaviour {
 
         for (var i = 0; i < tapCount; i++)
         {
-            var touch = Input.GetTouch(i);
-            Instantiate(spawnItem, new Vector3(touch.position.x,touch.position.y), Quaternion.identity);
+            var touch = Input.GetTouch(0);
+
+			//debugText.text = touch.position.x + " " + touch.position.y;
+			
+			Vector3 fingerPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x,touch.position.y));
+
+            Instantiate(spawnItem, fingerPos, Quaternion.identity);
             
         }
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(spawnItem, Camera.main.ScreenToWorldPoint(new Vector3(100,100)), Quaternion.identity);
+		}
 
 	}
 }
